ef36c2d0553d84021e1a5718758c5380c8a6fc66
Maxime <sin2.chabert.maxime@gmail.com>
2018-11-22 11:49:13 +0100

    Setup config eslint

---
+++ src/Containers/Layout/ItemsNavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import LayersIcon from '@material-ui/icons/Layers';
+import Divider from '@material-ui/core/es/Divider/Divider';
+
+export default (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <Divider />
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Développeurs" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Suivi des tâches" />
+    </ListItem>
+  </div>
+);
