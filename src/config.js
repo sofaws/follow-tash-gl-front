9ed55c4f9ad0665d8200c72eb42577859b79ae71
mchabert <mchabert@sqli.com>
2018-12-21 12:03:33 +0100

    Add little documentation to config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,23 @@
+/**
+ * Endpoint where glback API works
+ * @type {string}
+ */
 export const API_URL = "http://localhost:8080";
+
+/**
+ * List of different possible statuses for a task on your gitlab
+ * @type {string[]}
+ */
 export const STATUS = ["opened", "To Do", "Doing", "closed", "Blocked"];
+
+/**
+ * List of different types of tasks configured on your gitlab
+ * @type {string[]}
+ */
 export const TYPES = ["Front", "Back", "BugFix", "Autres"];
+
+/**
+ * Delays in minutes before each synchronization with the API
+ * @type {number}
+ */
 export const TIME_TO_RELOAD = 10;
