63d0fcb70e19fcabc724ffd16bab2b0a7741d536
maxime blanc <maximeblanc.dev@gmail.com>
2019-02-14 19:54:03 +0100

    ok

--- server/controllers/SheetController.js
+++ server/controllers/SheetController.js
@@ -9,14 +9,39 @@ class SheetController {
       (acc, { id, consumedTime, remainingTime }) => {
         const developpersKeys = Object.keys(consumedTime);
 
-        return [...acc, { id }];
+        const inputs = developpersKeys.reduce((secondAcc, developpersKey) => {
+          const input = consumedTime[developpersKey];
+          return [
+            ...secondAcc,
+            {
+              id,
+              remainingTime,
+              username: input.user.username,
+              time: input.time
+            }
+          ];
+        }, []);
+
+        return [...acc, ...inputs];
       },
       []
     );
     ctx.body = formatedTimes;
   }
 
-  getTasks() {}
+  getTasks(ctx) {
+    const issues = Issue.list();
+
+    const teamRegex = /[iI][lL][oOôÔ][tT] ([0-9]+)/i;
+    const lotRegex = /[lL][oO][tT] ([0-9]+).*/i;
+
+    const formatedTasks = issues.map(({ iid, title, state, labels }) => {
+      const team = labels.find(label => teamRegex.test(label));
+      const lot = labels.find(label => lotRegex.test(label));
+      return { id: iid, title, state, team, lot };
+    });
+    ctx.body = formatedTasks;
+  }
 }
 
 module.exports = new SheetController();
