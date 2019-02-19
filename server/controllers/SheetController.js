b5e4cb02a1895f244f3d742dc246fe3e9f148c4c
maxime blanc <maximeblanc.dev@gmail.com>
2019-02-19 17:30:54 +0100

    fix: add estimated time for tasks

--- server/controllers/SheetController.js
+++ server/controllers/SheetController.js
@@ -35,20 +35,23 @@ class SheetController {
     const issues = Issue.list();
     const times = Time.list();
 
-    const formatedTasks = issues.map(({ id, iid, title, state, labels }) => {
-      const team = labels.find(label => config.ILOTS.includes(label));
-      const lot = labels.find(label => config.LOTS.includes(label));
-      const time = times.find(time => time.id === id);
+    const formatedTasks = issues.map(
+      ({ id, iid, title, state, labels, estimatedTime }) => {
+        const team = labels.find(label => config.ILOTS.includes(label));
+        const lot = labels.find(label => config.LOTS.includes(label));
+        const time = times.find(time => time.id === id);
 
-      return {
-        id: iid,
-        title,
-        state,
-        team: team ? team : null,
-        lot: lot ? lot : null,
-        remainingTime: time ? time.remainingTime : null
-      };
-    });
+        return {
+          id: iid,
+          title,
+          state,
+          team: team ? team : null,
+          lot: lot ? lot : null,
+          remainingTime: time ? time.remainingTime : null,
+          estimatedTime
+        };
+      }
+    );
     ctx.body = formatedTasks;
   }
 }
