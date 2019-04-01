84248453f342a20477ca2ad3cbf5b09ee8a5c09a
Jeremy84 <mimytos@hotmail.fr>
2019-04-01 11:38:30 +0200

    Update ManagementHelper.js

--- src/utils/ManagementHelper.js
+++ src/utils/ManagementHelper.js
@@ -78,7 +78,7 @@ export function getCostOfListTasks(tasks) {
     return tasks.reduce((acc, task) => {
         if (!task.consumedTime) return acc;
         Object.values(task.consumedTime).forEach(element => {
-          if(typeof element.user === undefined){
+          if(element.length = 1 ){
             console.log("--------------------------------------------------------------------------------------");
             console.log(element);            
             console.log("--------------------------------------------------------------------------------------");
